c5ccb138f40626c2f97b153e97567c0602290110
Sarah Groff-Palermo <sarah.groff.palermo@gmail.com>
2014-03-02 19:59:35 -0800

    Regenerated JSON, background changes with feels

--- js/draw.js
+++ js/draw.js
@@ -14,6 +14,25 @@ function draw(){
     "grab bag": "hsla(39, 97%, 55%, 1)"
   };
 
+  var feels = {
+    'an affectionate': 'hsla(243, 93%, 64%, 1)',
+    'an ambivalent': 'hsla(243, 10%, 64%, 1)',
+    'an oppressed by the Western cannon': 'hsla(243, 10%, 27%, 1)',
+    'a cozy': 'hsla(243, 43%, 33%, 1)',
+    'a cringe-stalgic': 'hsla(109, 81%, 56%, 1)',   
+    'a curious': 'hsla(222, 90%, 33%, 1)',
+    'an empowered': 'hsla(6, 90%, 43%, 1)',
+    'an excited': 'hsla(6, 100%, 53%, 1)',
+    'a guilty': 'hsla(164, 100%, 22%, 1)',
+    'an introspective': 'hsla(164, 100%, 49%, 1)',
+    'a minimalist': 'hsla(0, 24%, 90%, 1)',
+    'a nostalgic': 'hsla(350, 100%, 88%, 1)',
+    'a proud': 'hsla(6, 90%, 43%, 1)',
+    'a wistful': 'hsla(239, 36%, 18%, 1)',
+    'an unrepentant': 'hsla(320, 100%, 49%, 1)',
+    'an indefinable': 'hsla(243, 10%, 52%, 1)'
+};
+
   var svg = d3.select('.chart')
     .append('svg')
     .attr('width', width)
@@ -27,16 +46,23 @@ function draw(){
   d3.json('js/books.json', function(error, data){
 
     data.forEach(function(d) {
+      
       d.pages = +d.pages;
     });
 
     rScale.domain([0, d3.max(data, function(d){return d.pages;})]);
 
-    var n = 0;
+    var n = Math.floor(Math.random() * (data.length + 1));
     var dataView = [data[n]];
 
     console.log(dataView);
-    
+
+    svg.append("rect")
+      .data(dataView)
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr("fill", function(d){ return feels[d.feeling]; });
+
     var chart = svg.selectAll('circle')
       .data(dataView)
       .enter()
@@ -45,6 +71,7 @@ function draw(){
       .attr('cy', height/2)
       .attr('r', function(d){return Math.sqrt(rScale(d.pages)/Math.PI);}) /*square root of pages normalized over pi*/
       .attr('fill', function(d){ return colors[d.genre]; });
+
 
     function changeBooks() {
 
@@ -55,56 +82,35 @@ function draw(){
 
       dataView = [data[n]];
 
-      // console.log('data-n', data[n]);
+      // Update background
 
-      // dataView.push(data[n]);
+       svg.insert("rect", "circle")
+         .data(dataView)
+         .attr("width", "100%")
+         .attr("height", "100%")
+         .attr("fill", function(d){ return feels[d.feeling]; });
 
-      console.log('dataview', dataView);
-
-      // dataView.shift();
-
-      
-      // console.log(dataView);
-
-      // Select + bind
+      // Update circle
 
       var bookCircles = svg.selectAll('circle')
         .data(dataView);
 
       bookCircles.transition()
         .duration(1000)
-        .attr('r', function(d){return Math.sqrt(rScale(d.pages)/Math.PI);}) /*square root of pages normalized over pi*/
+        .ease('elastic')
+        .attr('r', function(d, i){
+
+          var temp = Math.sqrt(rScale(d.pages)/Math.PI);
+          if (!(0 < temp && temp < 10000)) {
+            console.log(dataView);
+          }
+
+          return Math.sqrt(rScale(d.pages)/Math.PI);
+        }) /*square root of pages normalized over pi*/
         .attr('fill', function(d){ return colors[d.genre]; });
-
-      // // -- testing enter + exit ... --
-
-      // // Enter
-
-      // bookCircles.enter()
-      //   .append('circle')
-      //   .transition()
-      //   .duration(1000)
-      //   .attr('cx', width/2)
-      //   .attr('cy', height/2)
-      //   .attr('r', function(d){return Math.sqrt(rScale(d.pages)/Math.PI);}) /*square root of pages normalized over pi*/
-      //   .attr('fill', function(d){ return colors[d.genre]; });
-
-      // // No update, but ... EXIT
-
-      // bookCircles.exit()
-      //   .transition()
-      //   .duration(500)
-      //   .attr('r', 0)
-      //   .remove();
-
-      // -- OR can alternately transition single circle into new single data again ... --
-
-
-
     }
 
     setInterval(changeBooks, 1000);
-
   })
 }
 
