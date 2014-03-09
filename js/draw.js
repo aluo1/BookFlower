51ec27174d70dd7b593ac30f89d185c382ec184e
Sarah Groff-Palermo <sarah.groff.palermo@gmail.com>
2014-03-08 20:37:22 -0800

    Sounds selected but not playing

--- js/draw.js
+++ js/draw.js
@@ -15,22 +15,70 @@ function draw(){
   };
 
   var feels = {
-    'an affectionate': 'hsla(243, 93%, 64%, 1)',
-    'an ambivalent': 'hsla(243, 10%, 64%, 1)',
-    'an oppressed by the Western cannon': 'hsla(243, 10%, 27%, 1)',
-    'a cozy': 'hsla(243, 43%, 33%, 1)',
-    'a cringe-stalgic': 'hsla(109, 81%, 56%, 1)',   
-    'a curious': 'hsla(222, 90%, 33%, 1)',
-    'an empowered': 'hsla(6, 90%, 43%, 1)',
-    'an excited': 'hsla(6, 100%, 53%, 1)',
-    'a guilty': 'hsla(164, 100%, 22%, 1)',
-    'an introspective': 'hsla(164, 100%, 49%, 1)',
-    'a minimalist': 'hsla(0, 24%, 90%, 1)',
-    'a nostalgic': 'hsla(350, 100%, 88%, 1)',
-    'a proud': 'hsla(6, 90%, 43%, 1)',
-    'a wistful': 'hsla(239, 36%, 18%, 1)',
-    'an unrepentant': 'hsla(320, 100%, 49%, 1)',
-    'an indefinable': 'hsla(243, 10%, 52%, 1)'
+    'affectionate': { 
+        'color': 'hsla(243, 93%, 64%, 1)', 
+        'sound': 'sounds/affectionate.wav'
+    },
+    'ambivalent': {
+        'color': 'hsla(243, 10%, 64%, 1)',
+        'sound': 'sounds/ambivalent.wav'
+    },
+    'oppressed by the Western canon': {
+        'color': 'hsla(243, 10%, 27%, 1)',
+        'sound': 'sounds/oppressed.wav'
+    },
+    'cozy': {
+        'color': 'hsla(243, 43%, 33%, 1)',
+        'sound': 'sounds/cozy.wav'
+    },
+    'cringe-stalgic': {
+        'color': 'hsla(109, 81%, 56%, 1)',   
+        'sound': 'sounds/cringe.aif'
+    },
+    'curious': {
+        'color': 'hsla(222, 90%, 33%, 1)',
+        'sound': 'sounds/curious.wav'
+    },
+    'empowered': {
+        'color': 'hsla(6, 90%, 43%, 1)',
+        'sound': 'sounds/empowered.wav'
+    },
+    'excited': {
+        'color': 'hsla(6, 100%, 53%, 1)',
+        'sound': 'sounds/excited.wav'
+    },
+    'guilty': {
+        'color': 'hsla(164, 100%, 22%, 1)',
+        'sound': 'sounds/guilty.wav'
+    },
+    'introspective': {
+        'color': 'hsla(164, 100%, 49%, 1)',
+        'sound': 'sounds/introspective.wav'
+    },
+    'minimalist': {
+        'color': 'hsla(0, 24%, 90%, 1)',
+        'sound': 'sounds/minimalist.wav'
+    },
+    'nostalgic': {
+        'color': 'hsla(350, 100%, 88%, 1)',
+        'sound': 'sounds/nost-wist.wav'
+    },
+    'proud': {
+        'color': 'hsla(6, 90%, 43%, 1)',
+        'sound': 'sounds/proud.wav'
+    },
+    'wistful': {
+        'color': 'hsla(239, 36%, 18%, 1)',
+        'sound': 'sounds/nost-wist.wav'
+    },
+    'unrepentant': {
+        'color': 'hsla(320, 100%, 49%, 1)',
+        'sound': 'sounds/unrep.wav'
+    },
+    'indefinable': {
+        'color': 'hsla(243, 10%, 52%, 1)',
+        'sound': 'sounds/indef.wav'
+    }
 };
 
   var svg = d3.select('.chart')
@@ -43,10 +91,9 @@ function draw(){
   var rScale = d3.scale.linear()
     .range([0, Math.pow(smallest, 1.9)]);
 
-  d3.json('js/books.json', function(error, data){
-
+  d3.json('js/books_no-art.json', function(error, data){
     data.forEach(function(d) {
-      d.pages = +(d.pages.replace(/\,/g,''));
+      d.pages = +(d.pages.replace(/\,/g,'')); 
     });
 
     rScale.domain([0, d3.max(data, function(d){return d.pages;})]);
@@ -54,13 +101,11 @@ function draw(){
     var n = Math.floor(Math.random() * (data.length + 1));
     var dataView = [data[n]];
 
-    console.log(dataView);
-
     svg.append("rect")
       .data(dataView)
       .attr("width", "100%")
       .attr("height", "100%")
-      .attr("fill", function(d){ return feels[d.feeling]; });
+      .attr("fill", function(d){ return feels[d.feeling].color; });
 
     var chart = svg.selectAll('circle')
       .data(dataView)
@@ -86,11 +131,13 @@ function draw(){
       var bookCircles = svg.selectAll('circle')
         .data(dataView);
 
+      console.log(dataView);
+
       svg.selectAll('rect')
          .data(dataView)
          .attr("width", "100%")
          .attr("height", "100%")
-         .attr("fill", function(d){ return feels[d.feeling]; });  
+         .attr("fill", function(d){ return feels[d.feeling].color; });  
 
       bookCircles.transition()
         .duration(1000)
